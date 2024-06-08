7fc2c2222474b83cb37d422a5687ad98619fc692
Heisenburg <69845869+KevinA-cpu@users.noreply.github.com>
2024-06-08 20:15:29 +0700

    fix: import path

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { Meeting } from './meeting';
+import { Meeting } from 'src/entities/meeting';
 
 @Entity('users')
 export class User {
