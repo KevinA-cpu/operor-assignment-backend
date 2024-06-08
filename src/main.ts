2727521860a2e01fc2e08e2457bf84658a121263
Heisenburg <69845869+KevinA-cpu@users.noreply.github.com>
2024-06-08 21:14:03 +0700

    limit origin

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: 'https://operor-assignment-frontend.vercel.app/',
   });
   await app.listen(3000);
 }
